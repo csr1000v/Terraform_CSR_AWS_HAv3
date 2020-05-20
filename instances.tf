27a31cb18083ef82e3f5ffe7e4f2fad4edd3852b
Mohammad Ali Haider <mhaider34@gatech.edu>
2020-05-19 18:11:53 -0700

    Update instances.tf

--- instances.tf
+++ instances.tf
@@ -6,7 +6,7 @@ module instance1 {
   subnet_id            = "${aws_subnet.public1.id}"
   name                 = "csr1000v1"
   key_name             = aws_key_pair.csrkey.key_name
-  iam_instance_profile = "${aws_iam_instance_profile.csr1000v.name}"
+  iam_instance_profile = "${aws_iam_instance_profile.csr1000v[0].name}"
   #iam_instance_profile        = "${var.csr1000v_instance_profile != "" ? var.csr1000v_instance_profile : aws_iam_instance_profile.csr1000v[0].name}"
   associate_public_ip_address = true
   vpc_security_group_ids      = ["${module.security_group_outside.this_security_group_id}", "${module.ssh_security_group.this_security_group_id}"]
@@ -20,7 +20,7 @@ module instance2 {
   name                        = "csr1000v2"
   key_name                    = aws_key_pair.csrkey.key_name
   instance_type               = var.instance_type
-  iam_instance_profile        = "${aws_iam_instance_profile.csr1000v.name}"
+  iam_instance_profile        = "${aws_iam_instance_profile.csr1000v[0].name}"
   #iam_instance_profile        = "${var.csr1000v_instance_profile != "" ? var.csr1000v_instance_profile : aws_iam_instance_profile.csr1000v[0].name}"
   subnet_id              = aws_subnet.public2.id
   vpc_security_group_ids = ["${module.security_group_outside.this_security_group_id}", "${module.ssh_security_group.this_security_group_id}"]
