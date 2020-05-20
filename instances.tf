23989646473bb3664bb86597c501901397f11f77
Mohammad Ali Haider <mhaider34@gatech.edu>
2020-05-19 18:08:51 -0700

    made changes to IAM profile for instance
    used CSR1000v as the IAM profile

--- instances.tf
+++ instances.tf
@@ -1,13 +1,13 @@
 module instance1 {
-  source        = "terraform-aws-modules/ec2-instance/aws"
-  version       = "~> 2.0"
-  ami           = "${data.aws_ami.csr1000v.id}"
-  instance_type = var.instance_type
-  subnet_id     = "${aws_subnet.public1.id}"
-  name          = "csr1000v1"
-  key_name      = aws_key_pair.csrkey.key_name
-  #iam_instance_profile        = "${aws_iam_instance_profile.csr1000v.name}"
-  iam_instance_profile        = "${var.csr1000v_instance_profile != "" ? var.csr1000v_instance_profile : aws_iam_instance_profile.csr1000v[0].name}"
+  source               = "terraform-aws-modules/ec2-instance/aws"
+  version              = "~> 2.0"
+  ami                  = "${data.aws_ami.csr1000v.id}"
+  instance_type        = var.instance_type
+  subnet_id            = "${aws_subnet.public1.id}"
+  name                 = "csr1000v1"
+  key_name             = aws_key_pair.csrkey.key_name
+  iam_instance_profile = "${aws_iam_instance_profile.csr1000v.name}"
+  #iam_instance_profile        = "${var.csr1000v_instance_profile != "" ? var.csr1000v_instance_profile : aws_iam_instance_profile.csr1000v[0].name}"
   associate_public_ip_address = true
   vpc_security_group_ids      = ["${module.security_group_outside.this_security_group_id}", "${module.ssh_security_group.this_security_group_id}"]
 }
@@ -20,9 +20,10 @@ module instance2 {
   name                        = "csr1000v2"
   key_name                    = aws_key_pair.csrkey.key_name
   instance_type               = var.instance_type
-  iam_instance_profile        = "${var.csr1000v_instance_profile != "" ? var.csr1000v_instance_profile : aws_iam_instance_profile.csr1000v[0].name}"
-  subnet_id                   = aws_subnet.public2.id
-  vpc_security_group_ids      = ["${module.security_group_outside.this_security_group_id}", "${module.ssh_security_group.this_security_group_id}"]
+  iam_instance_profile        = "${aws_iam_instance_profile.csr1000v.name}"
+  #iam_instance_profile        = "${var.csr1000v_instance_profile != "" ? var.csr1000v_instance_profile : aws_iam_instance_profile.csr1000v[0].name}"
+  subnet_id              = aws_subnet.public2.id
+  vpc_security_group_ids = ["${module.security_group_outside.this_security_group_id}", "${module.ssh_security_group.this_security_group_id}"]
 }
 
 data "aws_ami" "csr1000v" {
